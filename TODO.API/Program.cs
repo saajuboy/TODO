a97f55165ae58458607e7a2a8140bf2f873998f9
saajidh <saajidhumar@gmail.com>
2022-03-06 09:39:28 +0530

    Configured Swagger

--- TODO.API/Program.cs
+++ TODO.API/Program.cs
@@ -6,14 +6,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c=>{
+    c.SwaggerDoc("v1",new Microsoft.OpenApi.Models.OpenApiInfo{Title="Todo API", Version="v1"});
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // Enable middleware to serve generated Swagger as a JSON endpoint.
     app.UseSwagger();
-    app.UseSwaggerUI();
+    // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+    // specifying the Swagger JSON endpoint.
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseHttpsRedirection();
