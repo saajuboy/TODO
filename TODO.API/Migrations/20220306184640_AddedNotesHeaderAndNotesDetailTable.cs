8e75f40b98996fb2fc19a8d1d3e4ba3d409607d6
saajidh <saajidhumar@gmail.com>
2022-03-07 23:24:39 +0530

    Migration For NotesHeader and Detail Table

---
+++ TODO.API/Migrations/20220306184640_AddedNotesHeaderAndNotesDetailTable.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todo.Migrations
+{
+    public partial class AddedNotesHeaderAndNotesDetailTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NotesHeaders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotesHeaders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NotesDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false),
+                    Archived = table.Column<bool>(type: "INTEGER", nullable: false),
+                    NotesHeaderId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NotesDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NotesDetails_NotesHeaders_NotesHeaderId",
+                        column: x => x.NotesHeaderId,
+                        principalTable: "NotesHeaders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NotesDetails_NotesHeaderId",
+                table: "NotesDetails",
+                column: "NotesHeaderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NotesDetails");
+
+            migrationBuilder.DropTable(
+                name: "NotesHeaders");
+        }
+    }
+}
