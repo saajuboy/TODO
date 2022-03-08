4c4243071c797846ed5437c9973921dfb32049b2
saajidh <saajidhumar@gmail.com>
2022-03-08 14:03:51 +0530

    Added Notes Controller

---
+++ TODO.API/Controllers/NotesController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TODO.API.Controllers
+{
+   
+    [ApiController]
+    [Route("[controller]")]
+    public class NotesController:ControllerBase
+    {
+        
+    }
+}
