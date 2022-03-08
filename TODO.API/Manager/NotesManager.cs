0733f11bab298f304b1eb2aa9242e720ab90a4d9
saajidh <saajidhumar@gmail.com>
2022-03-08 14:03:34 +0530

    Implemented Notes Manager Class

---
+++ TODO.API/Manager/NotesManager.cs
@@ -0,0 +1,43 @@
+using AutoMapper;
+using TODO.API.Data.Interfaces;
+using TODO.API.Dtos;
+using TODO.API.Dtos.Params;
+using TODO.API.Models;
+using TODO.API.Models.Helpers;
+
+namespace TODO.API.Manager
+{
+    public class NotesManager
+    {
+        private readonly INotesRepository _NotesRepository;
+        private readonly ITodoRepository _TodoRepository;
+        private readonly IMapper _Mapper;
+        public NotesManager(INotesRepository notesRepository, ITodoRepository todoRepository, IMapper mapper)
+        {
+            this._Mapper = mapper;
+            this._TodoRepository = todoRepository;
+            this._NotesRepository = notesRepository;
+        }
+
+        public async Task<PagedList<NoteDto>> GetNotes(NotesParam noteParam)
+        {
+            var NotesFromRepo = await _NotesRepository.GetNotes(noteParam);
+
+            var NotesToReturn = _Mapper.Map<PagedList<NoteDto>>(NotesFromRepo);
+
+            return NotesToReturn;
+        }
+
+        public async Task<NoteDto> AddNote(NoteDto noteToAdd)
+        {
+
+            var _noteToInsert = _Mapper.Map<NotesHeader>(noteToAdd);
+            _TodoRepository.Add(_noteToInsert);
+
+            var _result = await _TodoRepository.SaveAll();
+
+            var _noteToReturn = _Mapper.Map<NoteDto>(_noteToInsert);
+            return _noteToReturn;
+        }
+    }
+}
