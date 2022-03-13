4c47adf97129a7cd4964cfe72ec82dced82a0f36
saajidh <saajidhumar@gmail.com>
2022-03-13 20:54:05 +0530

    Configurations

--- TODO.SPA/src/app/app-routing.module.ts
+++ TODO.SPA/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TdNoteComponent } from './components/td-note/td-note.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: TdNoteComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
