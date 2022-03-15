66319210de3e8f4f623ac1ce9aa47ea6c6abd492
saajuboy <saajidhumar@gmail.com>
2022-03-15 23:58:19 +0530

    Added synchronus function and main note variables

--- TODO.SPA/src/app/components/td-note/td-note.component.ts
+++ TODO.SPA/src/app/components/td-note/td-note.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgbCalendar, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import * as moment from 'moment';
 import { Note } from 'src/app/models/note';
+import { NoteDetail } from 'src/app/models/note-detail';
 import { NoteParam } from 'src/app/models/note-param';
 import { NoteService } from 'src/app/services/note.service';
 
@@ -13,30 +14,54 @@ import { NoteService } from 'src/app/services/note.service';
 export class TdNoteComponent implements OnInit {
 
   notes: Note[] = [];
-
-
+  assignedNotes:NoteDetail[] = [];
+  activeNotes:NoteDetail[] = [];
+  onHoldNotes:NoteDetail[] = [];
+  completedNotes:NoteDetail[] = [];
+  
   model: NgbDateStruct;
-  date: {year: number, month: number};
-searchText:string;
-  constructor(private noteService: NoteService,private calendar: NgbCalendar) { }
+  searchText: string;
 
-  ngOnInit() {
+
+  constructor(private noteService: NoteService, private calendar: NgbCalendar) { }
+
+ async ngOnInit() {
     // let _date = moment().format('DD-YY-MM')
     let _date = moment().toISOString();
-    this.getNotes(_date);
+    // this.getNotes(_date);
+
+    this.notes = await this.getNote(_date);
+
+    console.log('await response',this.notes);
+    
   }
 
-  getNotes(date: string,searchString?:string,isArchived?:boolean) {
-    let _noteParam = new NoteParam();
-    _noteParam.date = date;
-    this.noteService.getNotes(_noteParam).subscribe(res => {
-      this.notes = res;
-      //validate or create
-    });
-  }
- 
+  // getNotes(date: string, searchString?: string, isArchived?: boolean) {
+  //   let _noteParam = new NoteParam();
+  //   _noteParam.date = date;
+  //   this.noteService.getNotes(_noteParam).subscribe(res => {
+  //     this.notes = res;
+  //     //validate or create
+  //   });
+  // }
+
   selectToday() {
     this.model = this.calendar.getToday();
   }
 
+
+  async getNote(date: string, searchString?: string, isArchived?: boolean): Promise<Array<Note>> {
+    let _noteParam = new NoteParam();
+    _noteParam.date = date;
+    return new Promise((resolve) => {
+      this.noteService.getNotes(_noteParam).subscribe({
+        next: (response) => {
+          let _notes: Note[] = response;
+          resolve(_notes);
+        }
+      });
+    })
+
+
+  }
 }
