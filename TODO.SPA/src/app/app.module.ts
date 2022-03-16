70c57b94e6c04a4b7d610337132f4fa83c8e734e
saajuboy <saajidhumar@gmail.com>
2022-03-17 00:16:14 +0530

    Added Material to MODULES

--- TODO.SPA/src/app/app.module.ts
+++ TODO.SPA/src/app/app.module.ts
@@ -4,14 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 //imports
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 //components
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TdNoteComponent } from './components/td-note/td-note.component';
 import { TdNoteCardComponent } from './components/td-note-card/td-note-card.component';
-import { FormsModule } from '@angular/forms';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     HttpClientModule,
     NgbModule,
     FormsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
