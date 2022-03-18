fedf2047630ce73bef0db79723fe3cd034a58f75
saajuboy <saajidhumar@gmail.com>
2022-03-19 00:23:36 +0530

    Added some ui elements

--- TODO.SPA/src/app/components/td-note-card/td-note-card.component.ts
+++ TODO.SPA/src/app/components/td-note-card/td-note-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { _Icons } from 'src/app/models/_Icons';
 
 @Component({
   selector: 'app-td-note-card',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TdNoteCardComponent implements OnInit {
 
+  _icons = new _Icons();
   constructor() { }
 
   ngOnInit() {
--- TODO.SPA/src/app/models/_Icons.ts
+++ TODO.SPA/src/app/models/_Icons.ts
@@ -1,6 +1,9 @@
-import { faCalendarDay, faHourglass } from "@fortawesome/free-solid-svg-icons";
+import { faArchive, faCalendarDay, faHourglass, faPencilAlt, faPlusSquare } from "@fortawesome/free-solid-svg-icons";
 
 export class _Icons {
     calendarDays = faCalendarDay;
     search = faHourglass;
+    plus = faPlusSquare;
+    archive = faArchive;
+    pencil = faPencilAlt;
 }
